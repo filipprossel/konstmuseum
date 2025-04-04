c1989de1abe0f6228f64ece57471cad731099582
Filip Prössel <filipprossel@hotmail.com>
2025-04-04 17:08:51 +0200

    yes

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Lara from '@primeng/themes/lara';
+import Material from '@primeng/themes/material';
 
 
 
@@ -19,10 +19,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Lara,
+        preset: Material,
         options: {
           // Stänger av darkmode
-          darkModeSelector: false || 'none'
+          darkModeSelector: false || 'none',
       }
       }
     })
