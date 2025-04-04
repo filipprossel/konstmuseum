78a307cb9e233ed8d34cde44a347ee82e749690d
Filip Prössel <filipprossel@hotmail.com>
2025-04-04 16:57:28 +0200

    darkmode dsiabled

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import Lara from '@primeng/themes/lara';
+
+
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,9 +18,13 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     providePrimeNG({
-        theme: {
-            preset: Aura
-        }
+      theme: {
+        preset: Lara,
+        options: {
+          darkModeSelector: false || 'none'
+      }
+      }
     })
   ],
 };
+
