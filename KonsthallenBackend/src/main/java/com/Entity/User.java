fb2dc739f15b7fe23f7142ab084d026e7765b6f8
Anton <antongronlund04@gmail.com>
2025-04-07 12:37:23 +0200

    Fixade Entity-user-klassen i java

--- KonsthallenBackend/src/main/java/com/Entity/User.java
+++ KonsthallenBackend/src/main/java/com/Entity/User.java
@@ -1,17 +1,27 @@
 package com.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 
 @Entity
+@Table(name="Users")
 public class User {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private int user_id;
+    private String first_name;
+    private String last_name;
+    private String date_of_birth;
+    private String email;
+    private String password;
+    private String join_date;
+    private String user_description;
+    private String last_online;
+    @JoinColumn
+    private int role_id;
+    private String user_pfp;
+
 
 
 }
