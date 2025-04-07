b3623dc5810f2b84243037f4cc0f677d437a9a87
raskai-3 <raskai-3@student.ltu.se>
2025-04-07 10:51:52 +0200

    Springy

---
+++ KonsthallenBackend/src/main/java/com/example/khbe/KhbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.khbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhbeApplication.class, args);
+    }
+
+}
---
+++ KonsthallenBackend/src/test/java/com/example/khbe/KhbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.khbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
