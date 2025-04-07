11561d86fb5cec6e3912bee24de10951045aa863
Anton <antongronlund04@gmail.com>
2025-04-07 10:33:28 +0200

    Backend - Spring boot

---
+++ KonstmuseumBackend/src/main/java/com/example/konstmuseumbackend/KonstmuseumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.konstmuseumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonstmuseumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonstmuseumBackendApplication.class, args);
+    }
+
+}
---
+++ KonstmuseumBackend/src/test/java/com/example/konstmuseumbackend/KonstmuseumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.konstmuseumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonstmuseumBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
