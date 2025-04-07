abc41105c225923b273392edcf83a120c481b2f9
raskai-3 <raskai-3@student.ltu.se>
2025-04-07 11:07:31 +0200

    Påbörjan på springy

---
+++ KonsthallenBackend/src/main/java/com/Entity/User.java
@@ -0,0 +1,15 @@
+package com.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class User {
+    @
+}
