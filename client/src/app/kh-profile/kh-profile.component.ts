0552ee7f7bce59b878813c191ab9bd3ba0f822b1
Antanon123 <antongronlund04@gmail.com>
2025-04-08 18:07:44 +0200

    Profilkomponent

---
+++ client/src/app/kh-profile/kh-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kh-profile',
+  imports: [],
+  templateUrl: './kh-profile.component.html',
+  styleUrl: './kh-profile.component.scss'
+})
+export class KhProfileComponent {
+
+}
