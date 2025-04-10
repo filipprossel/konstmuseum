6765d47e39175dd80066178d25f3329c4ca91dcc
Anton <antongronlund04@gmail.com>
2025-04-10 12:49:15 +0200

    Tog bort Service och pbörjade profil-implementering

--- client/src/app/kh-profile/kh-profile.component.ts
+++ client/src/app/kh-profile/kh-profile.component.ts
@@ -1,33 +1,15 @@
 import { Component} from '@angular/core';
-import { UserServiceService } from './kh-userService/user-service.service';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-kh-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   templateUrl: './kh-profile.component.html',
   styleUrl: './kh-profile.component.scss',
   providers: [HttpClient]
 })
 export class KhProfileComponent {
-
-  user: any;
-  currentUser: any;
-
-  constructor(private userService: UserServiceService){}
-  ngOnInit(): void {
-    this.userService.getData().subscribe((data) => {this.user = data;})
-  }
-  /*
-  findUser(): void {
-    this.currentUser = this.user[1]
-    for (let x in this.user){
-      if(this.currentUser === this.user[1].first_name){
-        console.log(this.user[1].first_name)
-      }
-    }*/
-
 }
