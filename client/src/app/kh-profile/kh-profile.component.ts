d673b0034ae6dc039b3994454b7a37556b498fa7
Antanon123 <antongronlund04@gmail.com>
2025-04-11 11:25:11 +0200

    fort. profil

--- client/src/app/kh-profile/kh-profile.component.ts
+++ client/src/app/kh-profile/kh-profile.component.ts
@@ -12,4 +12,44 @@ import { ButtonModule } from 'primeng/button';
   providers: [HttpClient]
 })
 export class KhProfileComponent {
+
+  disable(nextDocument: any): void{ 
+      let aktivitetContent = document.getElementById("aktivitetsContainer")!;
+      let profileContent: HTMLElement = document.getElementById("profileActivityContainer")!;
+      let omContent = document.getElementById("omContent")!;
+
+      let buttonA = document.getElementById("buttonA")!;
+      let buttonP = document.getElementById("buttonP")!;
+      let buttonO = document.getElementById("buttonO")!;
+
+
+      if(nextDocument === "aktivitet"){
+      profileContent.style.display = "none";
+      omContent.style.display = "none";
+      aktivitetContent.style.display = "flex";
+      buttonA.style.borderBottom = "solid"
+    }
+    else if(nextDocument === "profil"){
+      profileContent.style.display = "flex";
+      omContent.style.display = "none";
+      aktivitetContent.style.display = "none";
+    }
+    else{
+      profileContent.style.display = "none";
+      omContent.style.display = "flex";
+      aktivitetContent.style.display = "none";
+    }
+  }
+
+  aktivitetButton(): void{
+    this.disable("aktivitet")
+  }
+  omButton(): void{
+    this.disable("om")
+  }
+  porfileButton(): void{
+    this.disable("profil")
+  }
+
+  
 }
