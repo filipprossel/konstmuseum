169081f779449f2b3c8f3a1e4408c4b292c245e5
Filip Prössel <filipprossel@hotmail.com>
2025-05-06 10:54:00 +0200

    error "window" fix

--- client/src/app/kh-header/kh-header.component.ts
+++ client/src/app/kh-header/kh-header.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { AuthService } from '../auth.service';
 import { ProfileDropdownComponent } from "./profile-dropdown/profile-dropdown.component";
 import { KhLanguageSelectorComponent } from './kh-language-selector/kh-language-selector/kh-language-selector.component';
@@ -27,17 +27,19 @@ export class KhHeaderComponent implements OnInit {
 
   authService!: AuthService; 
 
-  constructor(authService: AuthService,private translocoService: TranslocoService) {
+  constructor(  @Inject(PLATFORM_ID) private platformId: Object,  authService: AuthService,private translocoService: TranslocoService) {
     this.authService = authService; 
   }
 
   ngOnInit(): void {
-    window.addEventListener('localLanguageChange', (event: any) => {
-      const newLang = event.detail?.newLang;
-      console.log('Language change', newLang);
-      this.translocoService.setActiveLang(newLang);
-      this.currentLangImg = getLanguageImagePath(newLang);
-    });
+    if(isPlatformBrowser(this.platformId)) {
+      document.addEventListener('localLanguageChange', (event: any) => {
+        const newLang = event.detail?.newLang;
+        console.log('Language change', newLang);
+        this.translocoService.setActiveLang(newLang);
+        this.currentLangImg = getLanguageImagePath(newLang);
+      });
+    }
     
     const user = this.authService.getUser(); 
     if (user) {
@@ -55,11 +57,11 @@ export class KhHeaderComponent implements OnInit {
   }
 
 
-  testLanguageSwitch() {
-    const currentLang = this.translocoService.getActiveLang();
-    const newLang = currentLang === 'swe' ? 'en' : 'swe';
-    this.currentLanguage = newLang;
-    this.translocoService.setActiveLang(newLang);
-    console.log(`Språk växlat till: ${newLang}`);
-  }
+  // testLanguageSwitch() {
+  //   const currentLang = this.translocoService.getActiveLang();
+  //   const newLang = currentLang === 'swe' ? 'en' : 'swe';
+  //   this.currentLanguage = newLang;
+  //   this.translocoService.setActiveLang(newLang);
+  //   console.log(`Språk växlat till: ${newLang}`);
+  // }
 }
